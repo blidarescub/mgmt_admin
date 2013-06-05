7d06f52fff84dadc49da8d99dd0aef984b58dcdd
blidarescub <bogdan.blidarescu@gmail.com>
2013-06-05 10:25:23 +0300

    revert to print

--- main_pop.py
+++ main_pop.py
@@ -25,13 +25,7 @@ def puppet_kick():
         task = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
         (stdout, stderr) = task.communicate()
-        while True:
-            out = task.stdout.read(1)
-            if out == '' and process.poll() != None:
-                break
-            if out != '':
-                sys.stdout.write(out)
-                sys.stdout.flush()
+        print stdout
         return stdout
     else:
         return "Incorrect or missing API key :("
